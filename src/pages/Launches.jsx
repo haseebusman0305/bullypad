53c5f18a6fb27807509770449d4179027c1b6cbe
Haseeb Usman <haseebusman030576@gmail.com>
2024-06-17 16:52:25 +0500

    chore: Update Launches page with styled select dropdown

--- src/pages/Launches.jsx
+++ src/pages/Launches.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
 import { Container } from '@mui/material';
-
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 const Lounches = () => {
     return (
-        <Container>
-            <Box>
-                <Typography variant="h1">
-                    New Launches
-                </Typography>
-                <Button component={Link} to="/launches" variant="contained" color="primary">
-                    Lounches
-                </Button>
-            </Box>
+        <Container className='px-1 p-7 font-bold text-4xl'>
+            <h1 className="px-1 py-7 font-bold text-4xl ">
+                LAUNCHES
+            </h1>
+            <FormControl className='w-44 h-10 border-none' sx={{ backgroundColor: '#212121' }}>
+                <InputLabel
+                    variant="standard"
+                    htmlFor="uncontrolled-native"
+                    sx={{ color: 'white'}}
+                >
+                    Select Exchange
+                </InputLabel>
+                <NativeSelect
+                    inputProps={{
+                        name: 'Raydium',
+                        id: 'uncontrolled-native',
+                    }}
+
+                    sx={{
+                        color: 'white',
+                       margin:'2rem',
+                        '&:before': {
+                            borderBottomColor: 'transparent',
+                        },
+                        '&:after': {
+                            borderBottomColor: 'transparent',
+                        },
+                        '& .MuiNativeSelect-icon': {
+                            color: 'white',
+                        },
+                    }}
+                >
+                    <option value='Raydium' className='text-white  bg-black'>Raydium</option>
+                </NativeSelect  >
+            </FormControl>
+
         </Container>
     );
 }
